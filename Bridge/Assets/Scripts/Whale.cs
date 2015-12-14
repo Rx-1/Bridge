d9b759bd18f54736912ab5425c23ab135c6c5c09
Rx-1 <roni.t.lipsunen@gmail.com>
2015-12-14 15:26:20 +0200

    Commit

--- Bridge/Assets/Scripts/Whale.cs
+++ Bridge/Assets/Scripts/Whale.cs
@@ -3,22 +3,70 @@ using System.Collections;
 
 public class Whale : MonoBehaviour {
 
+    public static int WhaleCount = 0;
+
+    enum WhaleState {Sunked, Surfacing, Surfaced, Diving};
+    WhaleState myState = WhaleState.Sunked;
+
+    public GameObject particlePrefab;
+
+    public float intialDepth = 10;
+    public float feedLevel = 0;
+    public float maxSpawnRadius = 120;
+    public float minSpawnRadius = 30;
+    public float speed = 10;
+    public float destructionDelay = 10;
+
+    bool readytofeed = false;
+
+    Transform player;
+
+    Animator AC;
+    Collider myCollider;
+
     void OnTriggerEnter(Collider c) {
-        if(c.isTrigger) {
-            transform.position += transform.up * 10;
-            Destroy(gameObject, 2);
-        } else {
-            Destroy(gameObject);
+        if ((!c.isTrigger || readytofeed) && (myState == WhaleState.Surfacing || myState == WhaleState.Surfaced)) {
+            if (c.isTrigger && readytofeed) {
+                GameObject particleXx = GameObject.Instantiate(particlePrefab);
+                particleXx.transform.position = transform.position;
+                Destroy(particleXx, destructionDelay);
+            }
+            Destroy(gameObject, destructionDelay);
+            WhaleCount--;
+            myState = WhaleState.Diving;
+            AC.SetBool("Dive", true);
         }
     }
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        AC = gameObject.GetComponentInChildren<Animator>(); 
+        myCollider = gameObject.GetComponent<Collider>();
+        player = ShipFunktions.player.transform;
+        WhaleCount++;
+        transform.position += Vector3.down * intialDepth;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if(myState == WhaleState.Sunked) {
+            if((transform.position - player.position).magnitude < maxSpawnRadius && (transform.position - player.position).magnitude > minSpawnRadius) {
+                myState = WhaleState.Surfacing;
+            }
+        } else if(myState == WhaleState.Surfacing) {
+            transform.LookAt(player.position);
+            transform.position += Vector3.up * speed * Time.deltaTime;
+            if(transform.position.y >= feedLevel) {
+                transform.position += Vector3.up * (feedLevel - transform.position.y);
+                myState = WhaleState.Surfaced;
+                AC.SetBool("Wake", true);
+            }
+        } else if (myState == WhaleState.Surfaced) {
+            transform.LookAt(player.position);
+            if (AC.GetCurrentAnimatorStateInfo(AC.GetLayerIndex("Base Layer")).IsName("MouthAgape"))
+                readytofeed = true;
+        } else {
+            transform.position -= Vector3.up * speed * Time.deltaTime;
+        }
 	}
 }
--- Bridge/Assets/Scripts/WhaleSpawner.cs
+++ Bridge/Assets/Scripts/WhaleSpawner.cs
@@ -5,12 +5,14 @@ public class WhaleSpawner : MonoBehaviour {
 
     public GameObject whalePrefab;
     public string ignoreLayer;
+    public int maxWhales = 10;
+
+    float depthXx;
+    float feedLevelXx;
 
     float radiusXx;
 
     int myLayerMask;
-
-    int i = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -21,20 +23,21 @@ public class WhaleSpawner : MonoBehaviour {
         radiusXx = whalePrefab.GetComponent<SphereCollider>().radius * whalePrefab. transform.lossyScale.x;
         if (!whalePrefab)
             Debug.LogError("Set whale prefab.");
+        depthXx = whalePrefab.GetComponent<Whale>().intialDepth;
+        feedLevelXx = whalePrefab.GetComponent<Whale>().feedLevel;
     }
 
     void TryToCreate() {
         Vector3 positionXx = new Vector3(Random.Range(LevelBounds.leftLimit, LevelBounds.rightLimit), 0, Random.Range(LevelBounds.bottomLimit, LevelBounds.topLimit));
-        if(!Physics.CheckSphere(positionXx, radiusXx, myLayerMask, QueryTriggerInteraction.Collide)) {
+        if(!Physics.CheckCapsule(positionXx + (Vector3.up * feedLevelXx), positionXx + (Vector3.down * depthXx), radiusXx, myLayerMask, QueryTriggerInteraction.Collide)) {
             GameObject whale = GameObject.Instantiate(whalePrefab);
             whale.transform.position = positionXx;
-            i++;
         }
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if(i < 100)
+        if(Whale.WhaleCount < maxWhales)
             TryToCreate();
 	}
 }
